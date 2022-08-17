c4c2e228abc82ad1f48ed853314261094d53f47e
Ruth4412-prog <kipchauijepkaa@gmail.com>
2022-08-17 04:38:05 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-
 // Write your code here
+  let num1=31;
+  let num2=2;
+  var multiply=62;
+  
+  let random=3;
+  let interger=true;
+
+
+  let mod=4;
+  let num3=25;
+  let num4=7;
+
+
+let max=20;
+(0,5,10,15,20);
